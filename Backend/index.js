b4a1771b60ab06700a6416e645ee0c31af66eb84
ayushsingh991399 <ayushsingh991399@gmail.com>
2024-05-16 19:29:58 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("./connection/connection.js");
 const app = express();
+const auth = require("./routes/auth.js");
 app.get("/",(req,res)=>{
     res.send("Hello World");
 });
+app.use(express.json());
+app.use("/api/v1",auth);
 app.listen(9000,()=>{
     console.log("Server is running on port 9000");
 });
---
+++ Backend/routes/auth.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const User = require("../Models/User");
+const bcrypt = require("bcrypt");
+//sign up
+router.post("/register",async (req,res)=>{
+    try {
+         const { email,username,password } = req.body;
+        const saltround = 10;
+        const hashpassword = await bcrypt.hash(password, saltround);
+         const user = new User({username,email,password:hashpassword});
+         await user.save().then(()=> res.status(200).json({user: user})
+    );
+    } catch(e){
+        res.status(400).json({message:"user Already exists"});
+    }
+});
+
+//signin
+router.post("/login",async (req,res)=>{
+    try {
+    const user = await User.findOne({email: req.body.email});
+    if(!user){
+        res.status(400).json({message:"email already exists"});
+    }
+
+    const ispasswordcorrect = await bcrypt.compare(
+        req.body.password,
+         user.password
+        );
+        if(!ispasswordcorrect){
+            res.status(400).json({message:"password is Not correct"});
+        }
+        const {password, ...others} = user._doc;
+        res.status(200).json({ user:others});
+
+    } catch(e){
+        res.status(400).json({message:"user Already exists"});
+    }
+});
+
+module.exports = router;
