adde8849c595739c155a054fd7a77b6999af361b
ayushsingh991399 <ayushsingh991399@gmail.com>
2024-05-22 15:11:20 +0530

    hello

--- Frontend/src/components/Todo/Todo.jsx
+++ Frontend/src/components/Todo/Todo.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./Todo.css";
 import TodoCards from "./TodoCards";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Update from "./Update";
 
 const Todo = () => {
     const[Inputs,setInputs] = useState({title:"" ,Description:""});
@@ -13,11 +16,29 @@ const Todo = () => {
        setInputs({...Inputs,[name]:value});
     };
     const submit = () => {
-        setArray([...Array, Inputs]);
+        if(Inputs.title === "" || Inputs.Description === ""){
+            toast.error("Title and Description Cannot be empty");
+        }else{
+            setArray([...Array, Inputs]);
         setInputs({ title: "",Description: ""});
+        toast.success("Your Task Is Added!");
+        toast.error("Your Task Is Not Saved ! Plase SignUp");
+        }
+        
+    };
+    const del = (id) => {
+       
+        Array.splice(id,"1");
+        setArray([...Array]);
+        toast.success("Your Task Is Deleted!");
+    };
+    const dis = (value) => {
+        document.getElementById("todo-update").style.display=value.toLowerCase();
     };
   return (
+    <>
     <div className="todo">
+        <ToastContainer />
         <div className="todo-main container d-flex justify-content-center align-items-center my-4 flex-column">
             <div className="d-flex flex-column todo-input-div w-50 p-1">
                 <input name="title" value={Inputs.title} onChange={change} onClick={show} type="text" className="my-2 p-2 todo-input" placeholder="Title" />
@@ -34,18 +55,28 @@ const Todo = () => {
             <div className="container-fluid">
                 <div className="row">
                         {Array && Array.map((item, index) =>(
-                            <div className="col-lg-3 mx-5 my-2">
-                                <TodoCards title={item.title} Description={item.Description}/>
+                            <div className="col-lg-3 col-10 mx-5 my-2 " key={index}>
+                                <TodoCards
+                                 title={item.title} 
+                                 Description={item.Description} 
+                                 id={index} 
+                                 delid={del}
+                                 display={dis}
+                                 />
                             </div>
                 ))}
-
-                    
                 </div>
                 
             </div>
 
         </div>
     </div>
+    <div className="Todo-update" id="todo-update">
+        <div className="container update">
+             <Update display={dis}/>
+        </div> 
+    </div>
+    </>
   );
 };
 
--- Frontend/src/components/Todo/TodoCards.jsx
+++ Frontend/src/components/Todo/TodoCards.jsx
@@ -1,5 +1,6 @@
-
-const TodoCards = ({title,Description}) => {
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { GrDocumentUpdate } from "react-icons/gr";
+const TodoCards = ({title,Description,id , delid, display}) => {
   return (
     <div className="p-3 todo-card">
       <div>
@@ -8,8 +9,23 @@ const TodoCards = ({title,Description}) => {
           {Description.split("",77)}...
         </p>
       </div>
+      <div className="d-flex justify-content-around ">
+        <div className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1"
+        onClick={()=>{
+          id
+          display("block");
+        }}>
+        <GrDocumentUpdate className="card-icons col-icon-del"/> Update
+        </div>
+         <div className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 text-danger"onClick={()=>{
+          delid(id);
+    
+         }}>
+        <RiDeleteBin6Fill className="card-icons col-icon-del"/> Delete
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default TodoCards;
